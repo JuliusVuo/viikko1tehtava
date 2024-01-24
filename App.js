dea32ac6143ec301c2a2a448fc51fa29c17f4125
Julius Vuorisalo <juliusvuorisalo@gmail.com>
2024-01-24 16:50:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,22 +3,24 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
-  const [age, setAge] = useState ('');
-  const [limits, setLimits] = useState (0);
+  const [age, setAge] = useState (0);
+  const [limits, setLimits] = useState ('');
 
 
 
 function calculate() {
-  const result = ((220-age) * 0.65) +"-"+ ((220-age) * 0.85)
+  const lowerLimit = (220-age) * 0.65
+  const upperLimit = (220-age) * 0.85
+  const result = lowerLimit.toFixed(0) +"-"+ upperLimit.toFixed(0)
   setLimits(result);
 }
 
   return (
     <View style={styles.container}>
       <Text style={styles.field}>Age</Text>
-      <TextInput style={styles.field} value={age} onChange={text => setAge(text)} keyboardType='numeric'/>
+      <TextInput style={styles.field} value={age} onChangeText={text => setAge(text)} keyboardType='numeric'/>
       <Text style={styles.field}>Limits</Text>
-      <Text style={styles.field}>{limits.toFixed(2)}</Text>
+      <Text style={styles.field}>{limits}</Text>
       <Button onPress={calculate} title= "Calculate"/>
 
     </View>
